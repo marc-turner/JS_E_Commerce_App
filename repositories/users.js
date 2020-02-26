e6b94e65c3bc2f34b61985b13dd48cf83e723a53
Marc Turner <marc.t1967@gmail.com>
2020-02-25 20:53:09 -0500

    finish Users repo

--- repositories/users.js
+++ repositories/users.js
@@ -87,16 +87,4 @@ class UsersRepository {
     }
 }
 
-// testing code - helper function
-const test = async () => {
-    const repo = new UsersRepository('users.json');
-
-    const user = await repo.getOneBy({
-        email: 'test@test.com',
-        password: 'password'
-    });
-
-    console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
