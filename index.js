c2bcf26a0b8e863061a3218fd326a12b088bb5d8
Marc Turner <marc.t1967@gmail.com>
2020-02-26 19:47:09 -0500

    add final basic signin,out routes

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ app.use(
     })
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
-            Your id is: ${req.session.userId}
             <form method="POST">
                 <input type='text' name='email' placeholder='email'/>
                 <input type='password' name='password' placeholder='password'/>
@@ -26,7 +25,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, confirmPassword } = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -42,6 +41,40 @@ app.post('/', async (req, res) => {
     req.session.userId = user.id;
 
     res.send('account created');
+});
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input type='text' name='email' placeholder='email'/>
+            <input type='password' name='password' placeholder='password'/>
+            <button>Sign In!</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+        return res.send('Email not found');
+    }
+
+    if (user.password != password) {
+        return res.send('Invalid Password');
+    }
+
+    req.sessionId = user.id;
+    res.send('You are signed in.');
 });
 
 app.listen(3000, () => {
