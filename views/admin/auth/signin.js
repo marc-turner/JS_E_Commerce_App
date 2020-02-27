7a3190bbf67878c89f23a56bdbe0f578db112806
Marc Turner <marc.t1967@gmail.com>
2020-02-27 12:32:41 -0500

    add signin and signup html layouts

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,13 @@
+const layout = require('../layout');
+
 module.exports = () => {
-    return `
-        <div>
-            <form method="POST">
-                <input type='text' name='email' placeholder='email'/>
-                <input type='password' name='password' placeholder='password'/>
-                <button>Sign In!</button>
-            </form>
-        </div>
-    `;
+    return layout({
+        content: `<div>
+                <form method="POST">
+                    <input type='text' name='email' placeholder='email'/>
+                    <input type='password' name='password' placeholder='password'/>
+                    <button>Sign In!</button>
+                </form>
+            </div>`
+    });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,15 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-    return `
-        <div>
-            Your id is: ${req.session.userId}
-            <form method="POST">
-                <input type='text' name='email' placeholder='email'/>
-                <input type='password' name='password' placeholder='password'/>
-                <input type='password' name='confirmPassword' placeholder='confirm password'/>
-                <button>Sign Up!</button>
-            </form>
-        </div>
-    `;
+    return layout({
+        content: `<div>
+                Your id is: ${req.session.userId}
+                <form method="POST">
+                    <input type='text' name='email' placeholder='email'/>
+                    <input type='password' name='password' placeholder='password'/>
+                    <input type='password' name='confirmPassword' placeholder='confirm password'/>
+                    <button>Sign Up!</button>
+                </form>
+            </div>`
+    });
 };
