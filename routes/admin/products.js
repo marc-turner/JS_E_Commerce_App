e7a64901a8350cf8e953456341ae0aa0fa33fd8a
Marc Turner <marc.t1967@gmail.com>
2020-03-01 11:51:18 -0500

    add npm multer

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../Repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ stroage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -12,11 +15,15 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
+router.post(
+    '/admin/products/new',
+    [requireTitle, requirePrice],
+    upload.single('image'),
+    (req, res) => {
+        const errors = validationResult(req);
 
-    res.send('Submitted');
-});
+        res.send('Submitted');
+    }
+);
 
 module.exports = router;
