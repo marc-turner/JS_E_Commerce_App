b5b9a52ab5c93653c1410443edfb12beed1476f3
Marc Turner <marc.t1967@gmail.com>
2020-03-05 13:27:22 -0500

    add styling to product edit page/form

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -2,15 +2,15 @@ const { validationResult } = require('express-validator');
 
 module.exports = {
     handleErrors(templateFunc, dataCb) {
-        return (req, res, next) => {
+        return async (req, res, next) => {
             const errors = validationResult(req);
 
             if (!errors.isEmpty()) {
                 let data = {};
-                if(dataCb) {
+                if (dataCb) {
                     data = await dataCb(req);
                 }
-                
+
                 return res.send(templateFunc({ errors, ...data }));
             }
 
