e32b868ecf67003781e2b43679507d4293c2516d
Marc Turner <marc.t1967@gmail.com>
2020-03-06 10:51:46 -0500

    add user facing products page styling

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cookieSession({
-    keys: ['lkasld235j']
-  })
+    cookieSession({
+        keys: ['lkasld235j']
+    })
 );
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
-  console.log('Listening');
+    console.log('Listening');
 });
